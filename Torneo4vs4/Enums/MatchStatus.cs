d1949d0725955f774a5366392b0c85b99245521a
Enrico Maria Sardellini <emsardellini@gmail.com>
2026-09-01 14:39:02 +0200

    Definizione struttura progetto

---
+++ Torneo4vs4/Enums/MatchStatus.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Torneo4vs4.Enums
+{
+    internal class MatchStatus
+    {
+    }
+}
---
+++ Torneo4vs4/Models/MatchPresence.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Torneo4vs4.Models
+{
+    internal class MatchPresence
+    {
+    }
+}
