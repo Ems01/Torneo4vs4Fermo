ca8fa574f5347f902c16fc079053905a7d717621
Enrico Maria Sardellini <emsardellini@gmail.com>
2026-09-01 14:17:52 +0200

    Add project files.

---
+++ Torneo4vs4/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Torneo4vs4
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
